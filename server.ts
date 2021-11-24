cfdb9cd8edad587a32a8f6562ad449983f9be075
hisham saymeh <hishamss90@gmail.com>
2021-11-24 15:49:45 -0700

    fixed express default route

--- server.ts
+++ server.ts
@@ -8,18 +8,13 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-    // this is default in case of unmatched routes in prod env
-    routes.use((req, res) => {
-        app.use(express.static(path.join(__dirname, '../client/build')));
-    });
-} else {
-    // this is default in case of unmatched routes ind dev env
-    routes.use((req, res) => {
-
-        res.redirect("http://localhost:3000/");
-    });
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+
+app.use(routes);
+
 (async () => {
     await sequelize.sync();
     app.listen(PORT, () => console.log(`Listning on port ${PORT}`));
@@ -27,6 +22,5 @@ if (process.env.NODE_ENV === 'production') {
 
 
 
-app.use(routes);
 
 
