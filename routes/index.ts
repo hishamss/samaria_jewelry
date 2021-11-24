eb8c2b97c79b94f4ec33baffe5a056986e1ed6a5
hisham saymeh <hishamss90@gmail.com>
2021-11-24 15:31:16 -0700

    fix default route for express

--- routes/index.ts
+++ routes/index.ts
@@ -5,17 +5,6 @@ const routes = Router();
 // API Routes
 routes.use("/api", apiRouters);
 
-if(process.env.NODE_ENV === 'production') {
-// this is default in case of unmatched routes in prod env
-routes.use((req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-});
-} else {
-  // this is default in case of unmatched routes ind dev env
-  routes.use((req, res) => {
-  
-    res.redirect("http://localhost:3000/");
-  });
-}
+
 
 export default routes;
